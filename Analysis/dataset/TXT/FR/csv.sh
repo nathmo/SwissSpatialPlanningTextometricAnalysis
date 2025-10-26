44ef83ad851dcabeab8f434c43fa3ce0a38beb73
Nathann Morand <natmo@hotmail.ch>
2025-10-26 23:28:30 +0100

    indexing was wrong, we must not include the file extension or TXM get confused...

--- Analysis/dataset/TXT/FR/csv.sh
+++ Analysis/dataset/TXT/FR/csv.sh
@@ -1,7 +1,8 @@
 echo "id,canton,language,year" > metadata.csv
 for f in *.txt; do
+    id="${f%.txt}"
     canton=$(echo "$f" | cut -d'_' -f2)
     lang="${f:0:2}"
     year=$(echo "$f" | cut -d'_' -f3 | cut -d'.' -f1)
-    echo "$f,$canton,$lang,$year" >> metadata.csv
+    echo "$id,$canton,$lang,$year" >> metadata.csv
 done
